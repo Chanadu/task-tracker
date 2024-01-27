8665103af14cd37df33814e0a9d27f0c8808de08
Chanadu <chandu.peddada@gmail.com>
2024-01-26 23:51:05 -0600

    router stuff

--- src/main.ts
+++ src/main.ts
@@ -7,11 +7,38 @@ import { firebaseApp } from './firebase/private_firebase/private_firebase';
 import IndexLayoutVue from './components/IndexLayout.vue';
 import ErrorNotFoundPageVue from './pages/ErrorNotFoundPage.vue';
 import IndexPageVue from './pages/IndexPage.vue';
+import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router';
 
 const pinia = createPinia();
+
+const routes: RouteRecordRaw[] = [
+	{
+		path: '/',
+		component: () => IndexLayoutVue,
+		children: [{ path: '', component: IndexPageVue }],
+	},
+	{
+		path: '/create-new-task',
+		component: () => IndexLayoutVue,
+		children: [{ path: '', component: IndexPageVue }],
+	},
+	// Always leave this as last one,
+	// but you can also remove it
+	{
+		path: '/:catchAll(.*)*',
+		component: () => ErrorNotFoundPageVue,
+	},
+];
+
+const router = createRouter({
+	// 4. Provide the history implementation to use. We are using the hash history for simplicity here.
+	history: createWebHashHistory(),
+	routes, // short for `routes: routes`
+});
+
 const app = createApp(App);
 app.use(pinia);
-
+app.use(router);
 app.use(VueFire, {
 	// imported above but could also just be created here
 	firebaseApp,
@@ -20,31 +47,5 @@ app.use(VueFire, {
 		VueFireAuth(),
 	],
 });
-
-// const routes: RouteRecordRaw[] = [
-// 	{
-// 		path: '/',
-// 		component: () => IndexLayoutVue,
-// 		children: [{ path: '', component: IndexPageVue }],
-// 	},
-// 	{
-// 		path: '/create-new-task',
-// 		component: () => IndexLayoutVue,
-// 		children: [{ path: '', component: IndexPageVue }],
-// 	},
-// 	// Always leave this as last one,
-// 	// but you can also remove it
-// 	{
-// 		path: '/:catchAll(.*)*',
-// 		component: () => ErrorNotFoundPageVue,
-// 	},
-// ];
-
-// const router = VueRouter.createRouter({
-// 	// 4. Provide the history implementation to use. We are using the hash history for simplicity here.
-// 	history: VueRouter.createWebHashHistory(),
-// 	routes, // short for `routes: routes`
-// });
-// app.use(router);
 
 app.mount('#app');
